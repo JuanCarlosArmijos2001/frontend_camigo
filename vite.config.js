bf0aa3b535ad94fd291d316af23917078c08d60b
JuanCarlosArmijos2001 <75904532+JuanCarlosArmijos2001@users.noreply.github.com>
2024-11-28 19:29:03 -0500

    Docker en local
    28/11/2024

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite que el servidor escuche conexiones externas
+    port: 5173,      // Define el puerto que estás exponiendo en Docker
+  },
 })
